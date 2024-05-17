281645743e735c48e6e04fe648c132ccbd886db6
Gubbiii <lucasrm@live.dk>
2024-05-17 11:48:00 +0200

    Storage type, items, statistic

---
+++ Rema1000LagerStyringsSystem/Models/Item.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rema1000LagerStyringsSystem.Models
+{
+    public class Item
+    {
+        private int _id = 1;
+        private string _name;
+        private static int _itemCounter;
+
+
+
+        public Item(string name) 
+        {
+            _id = _itemCounter;
+            _itemCounter++;
+
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+
+
+       
+
+    }
+
+}
---
+++ Rema1000LagerStyringsSystem/Models/StorageType.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rema1000LagerStyringsSystem.Models
+{
+    public class StorageType
+    {
+
+        private int _id = 1;
+        private string _name;
+        private static int _storageTypeCounter;
+
+
+
+
+
+        public StorageType(string name)
+        {
+            _id = _storageTypeCounter;
+            _storageTypeCounter++;
+
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+       
+
+
+    }
+}
