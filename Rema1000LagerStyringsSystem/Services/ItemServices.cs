dfb9711cd59e6908411ac9462c8b96fcb03ec367
IsmailNet <160605756+IsmailNet@users.noreply.github.com>
2024-05-17 12:45:01 +0200

    Update ItemServices.cs
    Item service

--- Rema1000LagerStyringsSystem/Services/ItemServices.cs
+++ Rema1000LagerStyringsSystem/Services/ItemServices.cs
@@ -1,12 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Rema1000LagerStyringsSystem.Services
+﻿namespace Rema1000LagerStyringsSystem.Services
 {
-    internal class ItemServices
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class ItemService
     {
+        private string fileName;
+        private Items Item;
+
+        
+        public ItemService(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        
+        public void CreateItem(Items item)
+        {
+            
+        }
+
+        
+        public void PrintItem(int id)
+        {
+            
+            
+        }
+
+        
+        public void UpdateItem(Items item)
+        {
+            
+        }
+
+        
+        public void RemoveItem(string name)
+        {
+            
+        }
+
+        
+        public Items GetItem(string name)
+        {
+
+            return null;
+        }
+
+        
+        public List<Items> GetItems()
+        {
+
+            return null;
+        } 
     }
 }
+ 
