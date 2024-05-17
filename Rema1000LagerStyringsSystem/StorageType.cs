1d912c9005e0d15e9aac6c41bfcec139a4c2bc27
Miki-Aqqalu Kruse <mstachkruse@gmail.com>
2024-05-17 10:19:39 +0200

    Updated classes

--- Rema1000LagerStyringsSystem/StorageType.cs
+++ Rema1000LagerStyringsSystem/StorageType.cs
@@ -6,9 +6,8 @@ using System.Threading.Tasks;
 
 namespace Rema1000LagerStyringsSystem
 {
-    public class ButiksHylde
+    public class StorageType
     {
-        //sadsadsad
 
     }
 }
--- Rema1000LagerStyringsSystem/WholeSaler.cs
+++ Rema1000LagerStyringsSystem/WholeSaler.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Rema1000LagerStyringsSystem
 {
-    public class WholeSale
+    public class WholeSaler
     {
 
     }
