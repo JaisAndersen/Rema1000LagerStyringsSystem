4b240ffb15b0baf73f17edb3531dbf7900e90300
FreddyBZealand <frb009@edu.zealand.dk>
2024-05-20 13:18:57 +0200

    prøver bare at opdatere

--- Rema1000LagerStyringsSystem/Services/StasticService.cs
+++ Rema1000LagerStyringsSystem/Services/StasticService.cs
@@ -9,5 +9,81 @@ namespace Rema1000LagerStyringsSystem.Services
     public class StatisticService
     {
 
+        private List<Customer> list;
+        
+
+
+        public void PrintItems()
+        {
+
+        }
+
+
+        public void PrintCustomers()
+        {
+
+        }
+        public void PrintStorageType()
+        {
+
+        }
+        public CustomerSearch(string name) Customer
+
+
+
+        public void GetItem(string name)
+        {
+
+        }
+            
+
+
+
+
+
+
+       
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        < Customer > list
+            <Colditem> list;
+            <Dryitem>list;
+        <ItemCategory> list;
+
+            +Printitem(int id):int 
+            +PrintCustomers():string;
+            +PrintItemCategory()void;
+            +CustomerSearch(string Name): Customer;
+            +GetItems()list <Items>;
+            +GetItem(string name);Item;
+
+
     }
 }
