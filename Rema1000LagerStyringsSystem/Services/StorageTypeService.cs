6e8307d1444d3c719400bfd0c3cb07673fc7a7f7
Gubbiii <lucasrm@live.dk>
2024-05-21 11:48:33 +0200

    asdasd

--- Rema1000LagerStyringsSystem/Services/StorageTypeService.cs
+++ Rema1000LagerStyringsSystem/Services/StorageTypeService.cs
@@ -9,6 +9,7 @@ namespace Rema1000LagerStyringsSystem.Services
 {
     public class StorageTypeService
     {
+        private StorageType StorageType = new StorageType("name");
         private List<Item> items;
         private string fileName = "";
         private List<StorageType> storageTypeList;
@@ -25,10 +26,42 @@ namespace Rema1000LagerStyringsSystem.Services
             get { return storageTypeList; }
         }
 
-        public void CreateStorageType() 
+        public void CreateStorageType(StorageType storageType) 
         {
+
+            storageTypeList.Add(storageType);
             
         }
+
+        public void RemoveStorageType(string Name)
+        {
+            StorageTypeList.RemoveAll(StorageType => StorageType.Name == Name);
+
+        }
+
+        public void GetStorageType(StorageType storageType)
+        {
+
+
+        }
+
+         public void UpdateStorageType(StorageType storageType)
+        {
+            if (storageType != null) 
+            { 
+              foreach (var e in StorageTypeList) 
+              {
+                    if (e.Name == storageType.Name)
+                    {
+                        e.Name = storageType.Name;
+                        e.Id = storageType.Id;
+                    }
+                    
+              }
+
+            }
+        }
+
         public void RemoveStorageType(int id) 
         {
             StorageTypeList.Remove(StorageTypeList[id]);
