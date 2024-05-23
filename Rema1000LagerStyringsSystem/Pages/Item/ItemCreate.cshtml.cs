116167afc565fcf9fb5777cbc159a3e922d246a3
FreddyBZealand <frb009@edu.zealand.dk>
2024-05-23 11:35:17 +0200

    ...

--- Rema1000LagerStyringsSystem/Pages/Item/ItemCreate.cshtml.cs
+++ Rema1000LagerStyringsSystem/Pages/Item/ItemCreate.cshtml.cs
@@ -1,16 +1,44 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Rema1000LagerStyringsSystem.Services;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using Rema1000LagerStyringsSystem.Models;
+using Microsoft.AspNetCore.Http.Features;
 
 namespace Rema1000LagerStyringsSystem.Pages.Itemcrud
 {
     public class ItemCreateModel : PageModel
     {
-        public void OnGet()
+        private IItem repo;
+        [BindProperty]
+        public Item item { get; set; }
+    public ItemCreateModel(Iitem repostitory) 
+        { 
+            repo = repostitory;
+        }
+        public IActionResult OnPost()
         {
-
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if (repo.GetAllItems().Count < Item.ItemId)
+            {
+                repo.AddPizza(Item);
+            }
+            repo.UpdateItem(Item);
+            return RedirectToPage("GetAllItems");
+        }
+        public IActionResult OnGet()
+        {
+                    return Page();
         }
 
 
+        }
 
     }
+
+
 }
