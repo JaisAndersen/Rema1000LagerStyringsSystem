1f991735cabae8c8409f806b730e3eba3ea4f198
Miki-Aqqalu Kruse <mstachkruse@gmail.com>
2024-05-23 09:42:08 +0200

    Update

--- Rema1000LagerStyringsSystem/Pages/Item/ItemPrint.cshtml.cs
+++ Rema1000LagerStyringsSystem/Pages/Item/ItemPrint.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Rema1000LagerStyringsSystem.Pages.Itemcrud
 {
-    public class ItemprintModel : PageModel
+    public class ItemPrintModel : PageModel
     {
         public void OnGet()
         {
