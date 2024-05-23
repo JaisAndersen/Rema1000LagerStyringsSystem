56291c372f31a3307a409f5b0833029702a36223
Miki-Aqqalu Kruse <mstachkruse@gmail.com>
2024-05-23 11:15:17 +0200

    update

---
+++ Rema1000LagerStyringsSystem/JsonHelpers/jsonfileWriterItem.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Schema;
+using Rema1000LagerStyringsSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+namespace Rema1000LagerStyringsSystem.JsonHelpers
+{
+    public class jsonfileWriterItem
+    {
+        public static void WriteToJson(List<Item> Items, string JsonFileName)
+        {
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(Items, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(JsonFileName, output);
+        }
+    }
+}
