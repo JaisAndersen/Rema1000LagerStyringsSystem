0051777a95c331d2fb3c1025fce1c5705c1836b3
Miki-Aqqalu Kruse <mstachkruse@gmail.com>
2024-05-24 12:01:30 +0200

    Restore

--- Rema1000LagerStyringsSystem/Services/ItemJson.cs
+++ Rema1000LagerStyringsSystem/Services/ItemJson.cs
@@ -7,7 +7,7 @@ namespace Rema1000LagerStyringsSystem.Services
 {
     public class ItemJson : IItem
     {
-        string fileName = @"C:\Users\mstac\source\repos\Rema1000LagerStyringsSystem\Rema1000LagerStyringsSystem\Data\jsonItems.json";
+        string fileName = @"C:\Users\mstac\source\repos\Rema1000LagerStyringsSystem\Rema1000LagerStyringsSystem\Rema1000LagerStyringsSystem\Data\jsonItems.json";
         private List<Item> Items;
         public void AddItem(Item item)
         {
