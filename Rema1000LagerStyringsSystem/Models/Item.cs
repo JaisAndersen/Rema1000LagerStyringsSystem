356acd3dbd37ebade2daec95ca0bdfdc17048322
Miki-Aqqalu Kruse <mstachkruse@gmail.com>
2024-05-24 10:26:35 +0200

    Update

--- Rema1000LagerStyringsSystem/Models/Item.cs
+++ Rema1000LagerStyringsSystem/Models/Item.cs
@@ -9,11 +9,15 @@ namespace Rema1000LagerStyringsSystem.Models
     {
         private int _id;
         private string _name;
+        private string _mærke;
+        private double _price;
 
         public Item() 
         {
             _name = Name;
             _id = Id;
+            _price = Price;
+            _mærke = Mærke;
         }
 
         public int Id
@@ -26,12 +30,19 @@ namespace Rema1000LagerStyringsSystem.Models
         public string Name
         {
             get { return _name; }
-
             set { _name = value; }
         }
 
-        public decimal Price { get; set; }
-       
+        public double Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+        public string Mærke 
+        {
+            get { return _mærke; }
+            set { _mærke = value; }
+        }
     }
 
 }
